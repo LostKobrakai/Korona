e44432c27203aca5c1e52c5a93378ca282ae0c30
Benni <benni@kobrakai.de>
2013-08-01 17:05:39 +0200

    First Commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>Korona</title>
+</head>
+<body>
+	<header>
+		<div class="topbar">
+			<div class="logo">
+				<img src="">
+			</div>
+			<div class="navigation">
+				<ul>
+					<li><a href="">Nav1</a></li>
+					<li><a href="">Nav2</a></li>
+					<li><a href="">Nav3</a></li>
+					<li><a href="">Nav4</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="teaser">
+			<img src="" alt="">
+			<h1>Headline</h1>
+		</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+	include("header.php");
+?>
+	<div class="breadcrump">
+		<ul>
+			<li>Home</li>
+			<li>Produkte</li>
+		</ul>
+	</div>
+	<div class="content">
+		<div class="produkte">
+			<div class="module">
+				<h2>Produkte</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum labore ea exercitationem tempore! Voluptas animi nobis in temporibus rerum obcaecati provident consectetur hic similique ullam. Quas neque possimus ab deleniti!</p>
+			</div>
+			<div class="module">
+				<h3>Systemprodukte</h3>
+				<img src="" alt="">
+			</div>
+			<div class="module">
+				<h3>Sonderanfertigungen</h3>
+				<img src="" alt="">
+			</div>
+		</div>
+	</div>
+<?php
+	include("footer.php");
+?>
