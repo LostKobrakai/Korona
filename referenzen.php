effecaf56746fafa7191810c9603aa21c6375ee8
Benni <benni@kobrakai.de>
2013-08-09 22:54:55 +0200

    Updated referenzen.php with real data

--- referenzen.php
+++ referenzen.php
@@ -4,79 +4,97 @@
 											"referenzen" => "referenzen.php");
 	include("header.php");
 ?>
-	<div class="content produkte grid">
-		<div class="block grid__item one-whole">
-			<h2 class="headline">Außenbeleuchtung</h2>
+	<div class="content produkte grid"><div class="block grid__item one-whole">
+			<h2 class="headline">Innenbeleuchtung</h2>
 			<div class="grid">
-				<div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="produkte_kor_01.php">kor_#01</a></h3>
+				<div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Forum Tashkent</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor01_produkt_uebersicht.jpg" alt=""></a>
-				</figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/forum_tashkent.jpg" alt=""></a>
+					</figure>
 				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Playmobil Funpark</a></h3>
+					<figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/playmobil_funpark.png" alt=""></a>
+					</figure>
+				</div><!--
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Rathaus Schorndorf</a></h3>
+					<figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/rathaus_schorndorf.jpg" alt=""></a>
+					</figure>
+				</div><!--
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Bloom Restaurant</a></h3>
+					<figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/bloom_restaurant.png" alt=""></a>
+					</figure>
+				</div><!--
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
 					<h3 class="headline"><a href="#">kor_#02</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor02_produkt_uebersicht.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/kirche_ka.png" alt=""></a>
 					</figure>
 				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="#">kor_#03</a></h3>
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Kobelkirche Westheim</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor03_produkt_uebersicht.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/kobelkirche_westheim.png" alt=""></a>
 					</figure>
 				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="#">kor_#04</a></h3>
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Akka Kirche</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor04_produkt_uebersicht.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/akka_kirche.jpg" alt=""></a>
 					</figure>
 				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="produkte_kor_01.php">kor_#05</a></h3>
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
+					<h3 class="headline"><a href="#">Loacker Kaffee</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor05_produkt_uebersicht.jpg" alt=""></a>
-					</figure>
-				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="#">kor_#06</a></h3>
-					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor06_produkt_uebersicht.jpg" alt=""></a>
-					</figure>
-				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="#">kor_magnLEDspot</a></h3>
-					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kormagnet_produkt_uebersicht.jpg" alt=""></a>
-					</figure>
-				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
-					<h3 class="headline"><a href="#">TailorLIGHT&reg;</a></h3>
-					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/kortailorlight_produkt_uebersicht2.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/innenbeleuchtung/loacker_kaffee.png" alt=""></a>
 					</figure>
 				</div>
 			</div>
 		</div><!--
-		 --><div class="block grid__item one-whole">
-			<h2 class="headline">Innenbeleuchtung</h2>
+		 -->
+		<div class="block grid__item one-whole">
+			<h2 class="headline">Außenbeleuchtung</h2>
 			<div class="grid">
-				<div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
-					<h3 class="headline"><a href="#">kor_#01</a></h3>
+				<div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+					<h3 class="headline"><a href="produkte_kor_01.php">ADAC Sendlinger Tor</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/sonder_01.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/aussenbeleuchtung/ADAC.jpg" alt=""></a>
+				</figure>
+				</div><!--
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+					<h3 class="headline"><a href="#">Playmobil Funpark</a></h3>
+					<figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/aussenbeleuchtung/Playmobil_outdoor.png" alt=""></a>
 					</figure>
 				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
-					<h3 class="headline"><a href="#">kor_#02</a></h3>
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+					<h3 class="headline"><a href="#">Linde</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/sonder_02.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/aussenbeleuchtung/linde_fassade.jpg" alt=""></a>
 					</figure>
 				</div><!--
-				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third sonderanfertigung">
-					<h3 class="headline"><a href="#">kor_#03</a></h3>
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+					<h3 class="headline"><a href="#">Brücke Heidelberg</a></h3>
 					<figure>
-						<a class="noarr" href="#"><img src="images/produkte_uebersicht/sonder_03.jpg" alt=""></a>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/aussenbeleuchtung/brueckeheidelberg.png" alt=""></a>
+					</figure>
+				</div><!--
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+					<h3 class="headline"><a href="produkte_kor_01.php">Alte Post</a></h3>
+					<figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/aussenbeleuchtung/alte_post.png" alt=""></a>
+					</figure>
+				</div><!--
+				 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+					<h3 class="headline"><a href="#">Medienfabrik</a></h3>
+					<figure>
+						<a class="noarr" href="#"><img src="images/referenzseite_uebersicht/aussenbeleuchtung/medienfabrik.jpg" alt=""></a>
 					</figure>
 				</div>
 			</div>
