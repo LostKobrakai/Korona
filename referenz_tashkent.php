34ade897800b08b09ecb67d3185331438c78292d
Benni <benni@kobrakai.de>
2013-08-09 15:00:23 +0200

    added referenz tashkent

---
+++ referenz_tashkent.php
@@ -0,0 +1,125 @@
+<?php
+	$h1 = "Forum Tashkent";
+	$breadcrumb = array("Home" => "index.php",
+											"Referenzen" => "referenzen.php",
+											"Innenbeleuchtung" => "referenzen.php#innen",
+											"Forum Tashkent" => "referenz-tashkent.php");
+	include("header.php");
+?>
+	<div class="content grid">
+		<div class="referenz">
+			<div class="grid__item">
+				<div class="grid">
+					<div class="p-images grid__item one-whole bp-large--two-thirds">
+						<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+						<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+					</div><!--
+					 --><div class="p-beschreibung grid__item one-whole bp-large--one-third">
+						<h2 class="headline">Beschreibung</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut delectus provident expedita ipsum in labore possimus ab obcaecati nobis saepe modi nemo impedit asperiores voluptatibus error perspiciatis dolorem commodi. Velit!</p>
+						<p>Vero expedita sequi voluptates repudiandae unde cum quae dignissimos officia natus sit minima soluta est laborum. Recusandae tenetur cupiditate sint quidem cumque dicta possimus ad ipsam eius numquam voluptatem itaque.</p>
+						<p>Odit facilis cum iste error inventore ipsum veniam accusantium quisquam laboriosam modi voluptate atque nisi adipisci ea quam quos accusamus facere fugit. Magni eum voluptatem ipsa autem modi debitis fugit?</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut delectus provident expedita ipsum in labore possimus ab obcaecati nobis saepe modi nemo impedit asperiores voluptatibus error perspiciatis dolorem commodi. Velit!</p>
+						<p>Vero expedita sequi voluptates repudiandae unde cum quae dignissimos officia natus sit minima soluta est laborum. Recusandae tenetur cupiditate sint quidem cumque dicta possimus ad ipsam eius numquam voluptatem itaque.</p>
+						<p>Odit facilis cum iste error inventore ipsum veniam accusantium quisquam laboriosam modi voluptate atque nisi adipisci ea quam quos accusamus facere fugit. Magni eum voluptatem ipsa autem modi debitis fugit?</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut delectus provident expedita ipsum in labore possimus ab obcaecati nobis saepe modi nemo impedit asperiores voluptatibus error perspiciatis dolorem commodi. Velit!</p>
+						<p>Vero expedita sequi voluptates repudiandae unde cum quae dignissimos officia natus sit minima soluta est laborum. Recusandae tenetur cupiditate sint quidem cumque dicta possimus ad ipsam eius numquam voluptatem itaque.</p>
+					</div>
+				</div>
+			</div>
+			<div class="p-images grid__item one-whole bp-large--two-thirds">
+				<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+				<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+				<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+				<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+				<img src="images/produkte_kor_01/kor01_produkt_gross.jpg" alt="">
+			</div>
+			<div class="block b-referenzprodukte grid__item one-whole">
+				<h2 class="headline">
+					<a href="#" class="inner">Produkte des Projektes</a>
+				</h2>
+				<div class="grid">
+					<div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third b-referenz">
+						<h3 class="headline">
+							<a href="#" class="inner">kor_#01</a>
+						</h3>
+						<figure><img src="images/referenz_tashkent/kor01_uebersicht.jpg" alt="Image">
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third b-referenz">
+						<h3 class="headline">
+							<a href="#" class="inner">kor_#05</a>
+						</h3>
+						<figure><img src="images/referenz_tashkent/kor05_uebersicht.jpg" alt="Image">
+						</figure>
+					</div>
+				</div>
+			</div>
+			<div class="block b-innenbeleuchtung grid__item one-whole">
+				<h2 class="headline">
+					<a href="#" class="inner">Alle Innenbeleuchtungen</a>
+				</h2>
+				<div class="grid">
+					<div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="produkte_kor_01.php">Digel</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+						<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="#">IOF</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="#">Lehmbachhaus</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="#">kor_#04</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="produkte_kor_01.php">kor_#05</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="#">kor_#06</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="#">kor_magnLEDspot</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div><!--
+					 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
+						<h3 class="headline"><a href="#">TailorLIGHT&reg;</a></h3>
+						<figure>
+							<a class="noarr" href="#"><img src="images/referenz_tashkent/rathaus_schorndorf.jpg" alt=""></a>
+							<figcaption>LED Profil, Stichpunkt, Produktbeschreibung, Stecker, Strombedingungen</figcaption>
+						</figure>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+	include("footer.php");
+?>
