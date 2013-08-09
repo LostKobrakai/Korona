d3568865c8e7aaca9259c90178af9724163f3c9d
Benni <benni@kobrakai.de>
2013-08-09 22:36:50 +0200

    Updated unternehmen.php with real data

--- unternehmen.php
+++ unternehmen.php
@@ -1,5 +1,5 @@
 <?php
-	$h1 = "Referenzen";
+	$h1 = "Unternehmen";
 	$breadcrumb = array("Home" => "index.php",
 											"Unternehmen" => "unternehmen.php");
 	include("header.php");
@@ -12,25 +12,25 @@
 		 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
 			<h3 class="headline"><a href="produkte_kor_01.php">Leistung</a></h3>
 			<figure>
-				<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor01_produkt_uebersicht.jpg" alt=""></a>
+				<a class="noarr" href="#"><img src="images/unternehmen/leistung.png" alt=""></a>
 		</figure>
 		</div><!--
 		 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third systemprodukt">
 			<h3 class="headline"><a href="#">Kompetenz</a></h3>
 			<figure>
-				<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor02_produkt_uebersicht.jpg" alt=""></a>
+				<a class="noarr" href="#"><img src="images/unternehmen/kompetenz.png" alt=""></a>
 			</figure>
 		</div><!--
 		 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third push--bp-large--one-third systemprodukt">
 			<h3 class="headline"><a href="#">Geschäftsführung</a></h3>
 			<figure>
-				<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor03_produkt_uebersicht.jpg" alt=""></a>
+				<a class="noarr" href="#"><img src="images/unternehmen/geschaeftsfuehrung.png" alt=""></a>
 			</figure>
 		</div><!--
 		 --><div class="block grid__item one-whole bp-small-2--one-half bp-large--one-third push--bp-large--one-third systemprodukt">
 			<h3 class="headline"><a href="#">Werkstatt</a></h3>
 			<figure>
-				<a class="noarr" href="#"><img src="images/produkte_uebersicht/kor04_produkt_uebersicht.jpg" alt=""></a>
+				<a class="noarr" href="#"><img src="images/unternehmen/werkstatt.png" alt=""></a>
 			</figure>
 		</div>
 	</div>
